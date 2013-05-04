c6081a6a22f9f65e84145299510f5e882fb8f385
Takashi Honda <takashi.0628.honda@gmail.com>
2013-05-04 11:56:35 +0900

    admin 修正

--- admin/api.php
+++ admin/api.php
@@ -5,11 +5,16 @@ require_once(dirname(__FILE__) . '/lib/util.php');
 $type = $_GET['type'];
 $limit = $_GET['limit'];
 $offset = $_GET['offset'];
+$id = null;
 if ($type == null) {
     $type = 'user';
 }
 
-$sql = Util::getSql($type);
+if ($type === 'help' && isset($_POST['id']) && Util::isNumber($_POST['id'])) {
+    $id = $_POST['id'];
+}
+
+$sql = Util::getSql($type, $id);
 
 $db = new DbWrap();
 Util::echoJSON($db->callMethod($sql, $limit, $offset));
--- admin/scripts/index.js
+++ admin/scripts/index.js
@@ -31,6 +31,14 @@ var firedLoad = function() {
     getAjax(AJAX_URL, 'GET', {'type': getURLHash(location.href)}, makeTable);
 }
 
+var firedClickSolve = function(target) {
+    getAjax(AJAX_URL + '?type=' + getURLHash(location.href), 'POST', {'id': target.name}, missingButton);
+}
+
+var missingButton = function(data, sendData) {
+    console.log('HERE');
+}
+
 var getAjax = function(sendUrl, sendType, sendData, callBackFunc) {
     // ingicater_start();
     $.ajax({
@@ -39,7 +47,7 @@ var getAjax = function(sendUrl, sendType, sendData, callBackFunc) {
         type: sendType,
         data: sendData,
         success: function(data) {
-            callBackFunc(data);
+            callBackFunc(data, sendData);
         },
         error: function(msg) {
             return errorAjax(msg);
@@ -54,19 +62,47 @@ var errorAjax = function(msg) {
     return false;
 }
 
-var makeTable = function(data) {
+var makeTable = function(data, sendData) {
     $('#main_table').text('');
+    var thead = makeThead(sendData['type']);
     var tbody = $('<tbody/>');
-
     for (var i = 0; i < data.length; i++) {
         var obj = data[i];
         var tr = $('<tr/>');
+        var isSoloved = false;
         for (var key in obj) {
             var td = $('<td/>');
             td.text(obj[key]);
             td.appendTo(tr);
+            if (key === 'is_solved' && obj[key] === '1') {
+                isSoloved = true;
+            }
+        }
+        if (sendData['type'] === 'help' && isSoloved === false) {
+            var td = $('<td/>');
+            var button = $('<button/>');
+            button.attr({'class': 'solved_button', 'name': obj['id']});
+            button.text('解決');
+            button.appendTo(td);
+            td.appendTo(tr);
         }
         tr.appendTo(tbody);
     }
+    thead.appendTo('#main_table');
     tbody.appendTo('#main_table');
+
+    $('.solved_button').on('click', function(e){
+        firedClickSolve(e.target);
+    });
+}
+
+var makeThead = function(type) {
+    var thead = $('<thead/>');
+    var tr = $('<tr/>');
+    for (var i = 0; i < HELP_THEAD[type].length; i++) {
+        var th = $('<th/>');
+        th.text(HELP_THEAD[type][i]);
+        th.appendTo(tr);
+    }
+    return tr;
 }
