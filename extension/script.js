a61c035cbf18e2771cb22b8ec3b8f1136990dce2
Kashima Takumi <kodokurider@gmail.com>
2013-05-04 13:44:33 +0900

    add post help priority

--- extension/script.js
+++ extension/script.js
@@ -1,6 +1,13 @@
 var element_login_form = document.querySelector('#login_form');
 var element_login_button = document.querySelector('#login_button');
 var element_user_name = document.querySelector('#user_name');
+var element_error_message = document.querySelector('#error_message');
+var element_post_form = document.querySelector('#post_form');
+var element_post_button = document.querySelector('#post_button');
+
+if (checkLogin() === true) {
+    afterLogin();
+}
 
 function loginByUserName(user_name) {
     var xhr = new XMLHttpRequest();
@@ -10,13 +17,15 @@ function loginByUserName(user_name) {
             console.log(xhr.responseText);
             var data = JSON.parse(xhr.responseText);
             if (data['code'] === 1) {
+                localStorage['helperUserName'] = user_name;
                 console.log('login success');
+                afterLogin();
             } else {
-                alert(data['msg']);
+                delete localStorage['helperUserName'];
+                element_error_message.textContent = data['msg'];
             }
         }
     }
-    // xmlHttpRequest.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
     var data = {user_name: user_name};
     console.log(data);
     xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
@@ -30,6 +39,45 @@ element_login_button.addEventListener('click', function(e) {
     e.stopPropagation();
     return false;
 }, false);
+
+function checkLogin() {
+    if (typeof(localStorage['helperUserName']) !== 'undefined' && 
+               localStorage['helperUserName']  !== null) {
+        return true;
+    }
+    return false;
+}
+
+function afterLogin() {
+    if (element_post_form.classList.contains('display_none') === true)  {
+        element_post_form.classList.remove('display_none');
+        element_login_form.classList.add('display_none');
+    }
+    element_post_button.addEventListener('click', function(e) {
+        var user_name = localStorage['helperUserName'];
+        var priority = document.querySelector('#post_form input[type="radio"]:checked').value;
+        postByUserNameAndPriority(user_name, priority);
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    }, false);
+}
+
+function postByUserNameAndPriority(user_name, priority) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', SERVER + '/help.php');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            console.log(xhr.responseText);
+            var data = JSON.parse(xhr.responseText);
+        }
+    }
+    var data = {user_name: user_name, help_priority: priority};
+    console.log(data);
+    xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+    xhr.send(encodeParams(data));
+}
+
 
 function encodeParams(data) {
     var params = [];
